652aa410e6e2cdaa54d89bbb41fa80f3a1df8fca
mrEsmaeili <lvlr.esmaeili@gmail.com>
2019-06-10 07:40:36 +0300

    User registration via token added plus some debug

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
-const User = require('../../modules/User');
+const User = require('../../models/User');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
