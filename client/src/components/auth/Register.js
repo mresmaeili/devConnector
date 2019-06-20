75373cf02b99f1a2df2ae89ad85cdb8e2399d48a
mrEsmaeili <lvlr.esmaeili@gmail.com>
2019-06-20 20:08:08 +0300

    login and register forms optimized for setting up edux

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -9,15 +10,15 @@ const Register = () => {
   });
   const { name, email, password, password2 } = formData;
 
-  const onChange = e =>
+  const onChange = async e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
       console.log('Passwords do not match');
     } else {
-      console.log(formData);
+      console.log('Success');
     }
   };
   return (
@@ -74,7 +75,7 @@ const Register = () => {
         <input type='submit' className='btn btn-primary' value='Register' />
       </form>
       <p className='my-1'>
-        Already have an account? <a href='login.html'>Sign In</a>
+        Already have an account? <Link to='/login'>Sign In</Link>
       </p>
     </Fragment>
   );
