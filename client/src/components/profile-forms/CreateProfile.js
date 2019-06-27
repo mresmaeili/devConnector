41dab93cf741ed7ee24d1000f6ca08460be073cf
mrEsmaeili <lvlr.esmaeili@gmail.com>
2019-06-27 16:06:36 +0300

    Create Profile Action added

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { useState, Fragment } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { createProfile } from '../../actions/profile';
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
   const [formData, setFormData] = useState({
     company: '',
     website: '',
@@ -36,6 +38,11 @@ const CreateProfile = props => {
   } = formData;
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    createProfile(formData, history);
+  };
   return (
     <Fragment>
       <h1 className='large text-primary'>Create Your Profile</h1>
@@ -44,9 +51,9 @@ const CreateProfile = props => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className='form'>
+      <form className='form' onSubmit={e => onSubmit(e)}>
         <div className='form-group'>
-          <select name='status'>
+          <select name='status' value={status} onChange={e => onChange(e)}>
             <option value='0'>* Select Professional Status</option>
             <option value='Developer'>Developer</option>
             <option value='Junior Developer'>Junior Developer</option>
@@ -90,7 +97,7 @@ const CreateProfile = props => {
             type='text'
             placeholder='Location'
             name='location'
-            value={website}
+            value={location}
             onChange={e => onChange(e)}
           />
           <small className='form-text'>
@@ -203,14 +210,19 @@ const CreateProfile = props => {
         )}
 
         <input type='submit' className='btn btn-primary my-1' />
-        <a className='btn btn-light my-1' href='dashboard.html'>
+        <Link className='btn btn-light my-1' to='/dashboard'>
           Go Back
-        </a>
+        </Link>
       </form>
     </Fragment>
   );
 };
 
-CreateProfile.propTypes = {};
+CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired
+};
 
-export default CreateProfile;
+export default connect(
+  null,
+  { createProfile }
+)(withRouter(CreateProfile));
